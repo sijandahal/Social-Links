38b724eb97a0c20427566684025779ed7f9c2475
sijandahal <dahalsijan7@gmail.com>
2024-01-28 23:34:14 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+        'color-primary' : 'hsl(75, 94%, 57%)',
+        'color-white' : 'hsl(0, 0%, 100%)',
+        'color-grey' : 'hsl(0, 0%, 20%)',
+        'color-darkgrey' : 'hsl(0, 0%, 12%)',
+        'color-offblack' : 'hsl(0, 0%, 8%)'
+    },
+  },
+  plugins: [],
+}
